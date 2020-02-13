2c3de071193b027605a4b8d39d7aeb9c71340f54
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-02-12 21:39:28 -0500

    added toggle menu classes function

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import './menu.css'
 
+const toggleMenuClasses = () => {
+	let hamburgerIcon = document.getElementById('burger')
+	hamburgerIcon.classList.toggle('is-active')
+	let mobileMenu = document.getElementById('menu-container')
+	mobileMenu.classList.toggle('opened')
+}
+
 const Menu = () => {
 	return (
 		<section>
-			<section className="burger">
+			<section className="burger" id="burger" onClick={toggleMenuClasses}>
 				<section className="lines">
 					<section className="line" />
 					<section className="line" />
 					<section className="line" />
 				</section>
 			</section>
-			<section className="menu-container">
+			<section className="menu-container" id="menu-container">
 				<section className="menu-logo">
-					<img className="menu-logo-img" src={require()} alt="logo" />
+					<img className="menu-logo-img" src={require('../../assets/7venthSunLogo.jpeg')} alt="logo" />
 				</section>
-				<section className="menu-title" />
+				<section className="menu-title">Menu Title</section>
 				<a className="menu-github" href="" target="_blank">
 					Source Code in Git
 				</a>
