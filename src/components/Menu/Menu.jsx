f5c94b3455326f259c60dfa7100e660cea15eb16
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-02-12 19:46:09 -0500

    setup menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './menu.css'
+
+const Menu = () => {
+	return (
+		<section>
+			<section className="burger">
+				<section className="lines">
+					<section className="line" />
+					<section className="line" />
+					<section className="line" />
+				</section>
+			</section>
+			<section className="menu-container">
+				<section className="menu-logo">
+					<img className="menu-logo-img" src={require('../assets/logo.img')} />
+				</section>
+				<section className="menu-title" />
+				<a className="menu-github" href="" target="_blank">
+					Source Code in Git
+				</a>
+				<a className="menu-twitter" href="" target="_blank">
+					Follow on Twitter
+				</a>
+			</section>
+		</section>
+	)
+}
+
+export default Menu
